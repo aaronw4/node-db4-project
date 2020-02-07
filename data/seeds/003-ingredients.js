a713bc6a7677d37143388db6d8acd8cc4ef8c690
Aaron Williams <aaronrw4@yahoo.com>
2020-02-06 20:04:15 -0800

    wrote ingredients seed

---
+++ data/seeds/003-ingredients.js
@@ -0,0 +1,8 @@
+
+exports.seed = function(knex) {
+  return knex('ingredients').insert([
+    {ingredientName: 'Bread'},
+    {ingredientName: 'Peanut Butter'},
+    {ingredientName: 'Jelly'}
+  ]);
+};
