96f3eb080bee2d6a4625d54b96b23eb80675882a
Aaron Williams <aaronrw4@yahoo.com>
2020-02-06 19:24:57 -0800

    created database with tables

--- knexfile.js
+++ knexfile.js
@@ -6,19 +6,20 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/recipes.db3'
-    }
-  },
-  migrations: {
-    directory: './data/migrations'
-  },
-  seeds: {
-    directory: './data/seeds'
-  },
-  // add the following
-  pool: {
-    afterCreate: (conn, done) => {
-      // runs after a connection is made to the sqlite engine
-      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+    },  
+    useNullasDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    // add the following
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
     },
   },
 }
